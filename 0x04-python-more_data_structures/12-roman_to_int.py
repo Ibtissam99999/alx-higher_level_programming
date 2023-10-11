274b345c73e01748af89b92aaa32400dec9e9239
Ibtissam99999 <ibtissamsahmoudi0@gmail.com>
2023-10-11 12:33:42 -0700

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,60 @@
 #!/usr/bin/python3
+
+def value(roman):
+    """returns value of each Roman symbol
+    """
+    if roman == 'I':
+        return 1
+    if roman == 'V':
+        return 5
+    if roman == 'X':
+        return 10
+    if roman == 'L':
+        return 50
+    if roman == 'C':
+        return 100
+    if roman == 'D':
+        return 500
+    if roman == 'M':
+        return 1000
+    return 0
+
+
 def roman_to_int(roman_string):
-    sum = 0
-    index = 0
-    if roman_string is not None:
-        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                 'C': 100, 'D': 500, 'M': 1000}
-        listrom = list(roman.keys())
-        length = len(roman_string)
-        for char in roman_string:
-            # print("++ "+char)
-            if char in roman:
-                # print("+ "+char, roman[char])
-                if char in 'IXC':
-                    if index < length-1:
-                        if listrom[index+1] in 'VXLCDM':
-                            sum = sum + (roman[listrom[index+1]]-roman[char])
-                sum += roman[char]
-            index += 1
-    return sum
+    """converts a Roman numeral to an integer
+    You can assume the number will be between 1 to 3999.
+    def roman_to_int(roman_string) must return an integer
+    If the roman_string is not a string or None, return 0
+    """
+    if not roman_string:
+        return 0
 
+    if not isinstance(roman_string, str):
+        return 0
 
-"""
-check if current charachter index is equal to lenght
-"I" can be subtracted from "V" and "X",
-"X" can be subtracted from "L" and "C",
-"C" can be subtracted from "D" and "M".
-However, "V", "L", and "D" cannot be used to represent subtraction.
-"""
+    result = 0
+    i = 0
+
+    while i < len(roman_string):
+        # getting value of symbol roman_string[i]
+        str1 = value(roman_string[i])
+
+        if i + 1 < len(roman_string):
+            # getting value of symbol roman_string[i + 1]
+            str2 = value(roman_string[i + 1])
+
+            # comparing both values
+            if str1 >= str2:
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                result = result + str1
+                i = i + 1
+            else:
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                result = result + str2 - str1
+                i = i + 2
+        else:
+            result = result + str1
+            i = i + 1
+    return result
